2b0c05dbb42ef1e1107fb4314a246feb3df1a303
Preedee Ponchevin <nickmsft@mac.com>
2023-09-30 08:20:38 +0700

    Update  - Enhance code split modules

--- src/main_exflow_runtime.rs
+++ src/main_exflow_runtime.rs
@@ -1,64 +1,24 @@
-use std::thread::sleep;
-use std::time::Duration;
-use crate::mod_azure::azure::{adf_pipelines_get, adf_pipelines_run};
+use actix_web::{App, HttpServer, middleware, web};
+use actix_web::middleware::Logger;
 use log::{debug, error, info};
-use crate::mod_azure::entities::ADFPipelineRunStatus;
+use crate::mod_runtime_api::runtime_api::{get_status_pipeline, post_run_pipeline};
 
 mod mod_azure;
+mod mod_runtime_api;
 
-#[tokio::main]
-async fn main() {
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
     pretty_env_logger::init();
-
-    let subscription_id = "2ad6d4fd-dcef-4a30-86c7-becd50d38034";
-    let resource_group_name = "NICK-RG-SEA-001";
-    let factory_name = "DevFactory001";
-    let pipeline_name = "pipeline_parallel_function";
-
-    let res = adf_pipelines_run(
-        subscription_id,
-        resource_group_name,
-        factory_name,
-        pipeline_name,
-    )
-    .await;
-    match res {
-        Ok(x) => {
-            debug!("{:#?}", x);
-            loop {
-                sleep(Duration::from_secs(10));
-                let res_status = adf_pipelines_get(
-                    subscription_id,
-                    resource_group_name,
-                    factory_name,
-                    x.run_id.as_str(),
-                ).await;
-                match res_status {
-                    Ok(status) => {
-                        match status.status.clone().unwrap() {
-                            ADFPipelineRunStatus::Queued |
-                            ADFPipelineRunStatus::InProgress => {
-                                info!("Status : {:#?}", status.status.unwrap());
-                            }
-                            ADFPipelineRunStatus::Succeeded |
-                            ADFPipelineRunStatus::Failed |
-                            ADFPipelineRunStatus::Canceling |
-                            ADFPipelineRunStatus::Cancelled => {
-                                info!("Status : {:#?}", status.status.unwrap());
-                                break
-                            }
-                        }
-                    }
-                    Err(er) => {
-                        error!("Get status error : {:#?}", er);
-                        break
-                    }
-                }
-            }
-
-        }
-        Err(x) => {
-            error!("{:#?}", x);
-        }
-    }
+    debug!("ExFlow Runtime starting....");
+    HttpServer::new(|| {
+        App::new()
+            .wrap(Logger::default())
+            .wrap(Logger::new("%a %{User-Agent}i"))
+            .wrap(middleware::DefaultHeaders::new().add(("X-Version", "0.1")))
+            .service(web::scope("/api/v1")
+                .route("/run_pipeline",web::post().to(post_run_pipeline))
+                .route("/get_status",web::get().to(get_status_pipeline))
+        )
+    }).workers(10)
+        .bind(("0.0.0.0",8082))?.run().await
 }
---
+++ src/mod_runtime_api/mod.rs
@@ -0,0 +1,3 @@
+pub mod entities;
+pub mod runtime_api;
+pub mod runtime_cli;
