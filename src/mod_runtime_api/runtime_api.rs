8923d583ac98f534ee53dc4298e12f33691dc150
Preedee Ponchevin <nickmsft@mac.com>
2023-10-07 18:09:17 +0700

    Add  - Add callback function for run_process in runtime web

--- src/mod_runtime_api/runtime_api.rs
+++ src/mod_runtime_api/runtime_api.rs
@@ -1,9 +1,21 @@
 use crate::mod_runtime_api::entities::PipelineRunRequest;
 use actix_web::{web, HttpResponse, Responder};
 use tracing_attributes::instrument;
+use log::info;
 
+use crate::run_process;
 #[instrument]
-pub async fn post_run_pipeline(_request: web::Json<PipelineRunRequest>) -> impl Responder {
+pub async fn post_run_pipeline(request: web::Json<PipelineRunRequest>) -> impl Responder {
+    //
+    // Call run_process is same CLI
+    //
+    let _result = run_process(&request.subscription_id,
+    &request.resource_group_name,
+        &request.factory_name,
+        &request.pipeline_name,3u64,Some(Box::new(|resp|{
+            info!("{:#?}",resp);
+        }))).await;
+    //result.unwrap().join_handle.join().expect("TODO: panic message");
     HttpResponse::Ok().finish()
 }
 #[instrument]
