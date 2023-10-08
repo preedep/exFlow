63b6a2da2fb9e2fda4050f53fd3f13b49fa4135f
Preedee Ponchevin <nickmsft@mac.com>
2023-10-08 09:41:22 +0700

    Add  - ExFlow Scheduler

--- src/main_exflow_scheduler.rs
+++ src/main_exflow_scheduler.rs
@@ -1,6 +1,24 @@
-use clokwerk::AsyncScheduler;
+use std::time::Duration;
+use clokwerk::{AsyncScheduler, Job, TimeUnits};
 
-fn main(){
 
+
+#[tokio::main]
+async fn main(){
+
+    // Create a new scheduler
     let mut scheduler = AsyncScheduler::new();
+// Add some tasks to it
+    scheduler
+        .every(10.minutes())
+        .plus(30.seconds())
+        .run(|| async { println!("Simplest is just using an async block"); });
+
+// Or spawn a task to run it forever
+    tokio::spawn(async move {
+        loop {
+            scheduler.run_pending().await;
+            tokio::time::sleep(Duration::from_millis(100)).await;
+        }
+    });
 }
